fcfd045eaac1d25be664c23e7425e8a37d350906
Olyno <alexlew95@gmail.com>
2020-04-22 23:21:40 +0200

    :art: Format code

--- src/koa/index.ts
+++ src/koa/index.ts
@@ -7,7 +7,7 @@ export default (opts?: KoporeiConfig) => {
     Koporei(opts);
     return async (ctx: Context, next) => {
         const result: KopereiRoute[] = routes.filter(
-            route => route.path === ctx.url,
+            (route) => route.path === ctx.url,
         );
         if (result.length > 0) {
             if (ctx.method.toUpperCase() === 'GET') {
@@ -15,9 +15,10 @@ export default (opts?: KoporeiConfig) => {
                 ctx.body = result[0].method.GET as string;
             } else if (ctx.method.toUpperCase() === 'POST') {
                 if (result[0].method.POST) {
-                    const callback: 
-                        (ctx: Context, next) => Promise<void> | void
-                            = result[0].method.POST;
+                    const callback: (
+                        ctx: Context,
+                        next,
+                    ) => Promise<void> | void = result[0].method.POST;
                     return callback(ctx, next);
                 }
             }
